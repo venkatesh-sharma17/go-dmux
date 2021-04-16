8af6c16c72a65e888656e6b676e682a29fb5c8ca
Devendra Mandan <devendra.mandan@flipkart.com>
2021-04-16 12:31:22 +0530

    changes for internal k8s

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-mkdir -p  /var/log/go-dmux
+mkdir -p  /var/log/flipkart/go-dmux
+
+#if [[ -z ${LOG_DIR} ]]; then
+#  export LOG_DIR="/var/log/go-dmux"
+#  mkdir -p ${LOG_DIR}
+#fi
 sleep 10
-/app/go-dmux /app/config/conf.json > /var/log/go-dmux/stdout.log 2> /var/log/go-dmux/stderr.log
+/app/go-dmux /app/config/conf.json
