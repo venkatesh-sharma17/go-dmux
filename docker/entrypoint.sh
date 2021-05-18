90ef84f94296a4635f7bf9fac2d9907b7e8b0789
Devendra Mandan <devendra.mandan@flipkart.com>
2021-05-18 12:04:18 +0530

    review changes

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-mkdir -p  /var/log/flipkart/go-dmux
-
-#if [[ -z ${LOG_DIR} ]]; then
-#  export LOG_DIR="/var/log/go-dmux"
-#  mkdir -p ${LOG_DIR}
-#fi
-sleep 10
 /app/go-dmux /app/config/conf.json
