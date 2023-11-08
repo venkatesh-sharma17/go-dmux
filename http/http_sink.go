1f7b4354e05b8a6c0f9a184f5f54fb6aa5e1bce0
venkatesh.sharma <venkatesh.sharma@flipkart.com>
2023-11-08 15:02:30 +0530

    new bufeer for oms calls

--- http/http_sink.go
+++ http/http_sink.go
@@ -53,6 +53,8 @@ func getHTTPClientTransport(size int, conf HTTPSinkConf) http.RoundTripper {
 		IdleConnTimeout:       90 * time.Second,
 		TLSHandshakeTimeout:   10 * time.Second,
 		ExpectContinueTimeout: 1 * time.Second,
+		WriteBufferSize:       100,
+		ReadBufferSize:        100,
 	}
 
 }
